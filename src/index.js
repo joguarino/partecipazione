e15423930ec0cea1a482c35415a545ddcae409f7
Giovanni Guarino <g.guarino@teamsystem.com>
2021-03-02 22:51:45 +0100

    feat(hawn): logo

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ $(() => {
 	$('#font').on('change', (event) => {
 		var font = event.target.value;
 		$('#partecipazione').removeClass().addClass(font);
+
 		$('#piega').removeClass().addClass(font);
-		$('#font-numeri').val(font);
 	});
 
 	$('#font-numeri').on('change', (event) => {
